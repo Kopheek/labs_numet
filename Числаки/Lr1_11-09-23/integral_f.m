2957c6a6d3da33f8266140789441e60efecb5ba2
Vladimir Ermak <VovchikShow.vs@gmail.com>
2023-12-15 15:04:13 +0300

    Что-то докодилось, что хз :smirk:

--- Числаки/Lr1_11-09-23/integral_f.m
+++ Числаки/Lr1_11-09-23/integral_f.m
@@ -1,6 +1,8 @@
 function [ y ] = integral_f( x )
-% y = cos(x^2); % (Софа)
-% y = x*cos(x^3); % (Дема)
-y = sin(x)*exp(1)^((-x)^2); % (Вова)
+    % y = cos(x^2); % (Софа)
+    % y = x*cos(x^3); % (Дема)
+    % y = sin(x)*exp(1)^((-x)^2); % (Вова)
+    y = exp(cos(x)); % (Миша)
 end
-
+    
+    
--- Числаки/Lr3_09-10-23/dfunctnelur.m
+++ Числаки/Lr3_09-10-23/dfunctnelur.m
@@ -1,5 +1,6 @@
 function [ df ] = dfunctnelur(x, f)
     %df = (2^x)*log(2)-2*x; из учебника
     %df = 2*x + 2^x*log(2); % Вова
-    df = 4^x*log(4) - 5; % Софа
+    %df = 4^x*log(4) - 5; % Софа
+    df = 2*x + 2^x*log(2); % Миша
 end
